0c46127f92998db25fbe5f1bee906b061d163130
Kiril Savino <kiril.savino@gmail.com>
2023-02-08 03:25:57 -0500

    skip a lot

--- Home.tsx
+++ Home.tsx
@@ -1,12 +1,16 @@
-import * as React from "react";
-import { View, Text, Button, Image, TouchableOpacity } from "react-native";
-import { FakeNav } from "./Types";
+import * as React from "react"
+import { View, Text, Button, Image, TouchableOpacity } from "react-native"
+import { FakeNav } from "./Types"
 
 interface Props {
     navigation: FakeNav
 }
 
 export default function HomeScreen({ navigation }: Props) {
+    const createWallet = () => {
+        navigation.navigate('Dashboard')
+    }
+
     return (
         <View style={{ flex: 1, backgroundColor: "#ef390f" }}>
             <View style={{ flex: 1, position: 'absolute', width: '100%', marginTop: 100, alignItems: 'center' }}>
@@ -18,8 +22,8 @@ export default function HomeScreen({ navigation }: Props) {
                 <Text style={{ marginTop: 100, fontSize: 50, alignContent: 'center', marginHorizontal: 50, textAlign: 'center', fontFamily: 'AkzidenzGroteskBQ-BdCnd', color: 'white' }}>GET PAID FOR YOUR DATA</Text>
             </View>
             <View style={{ position: 'absolute', width: '100%', justifyContent: 'center', bottom: 0, marginBottom: 30}}>
-                <TouchableOpacity style={{ height: 50, alignItems: 'center', justifyContent: 'center', marginHorizontal: 60, backgroundColor: 'white', borderRadius: 25 }} onPress={() => navigation.navigate('CreateWallet')}>
-                    <Text style={{ fontSize:15, alignItems: 'center', justifyContent: 'center', fontFamily: 'AkzidenzGroteskBQ-Reg', color: '#ef390f', fontWeight: 'bold' }}>CREATE A WALLET</Text>
+                <TouchableOpacity style={{ height: 50, alignItems: 'center', justifyContent: 'center', marginHorizontal: 60, backgroundColor: 'white', borderRadius: 25 }} onPress={createWallet}>
+                    <Text style={{ fontSize:15, alignItems: 'center', justifyContent: 'center', fontFamily: 'AkzidenzGroteskBQ-Reg', color: '#ef390f', fontWeight: 'bold' }}>Create Wallet</Text>
                 </TouchableOpacity>
             </View>
       </View>
