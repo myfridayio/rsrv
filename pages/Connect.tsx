e13ec88b740350649fd69db5e92c75855e5f37c4
Kiril Savino <kiril.savino@gmail.com>
2023-03-12 23:02:30 -0400

    working-ish

--- pages/Connect.tsx
+++ pages/Connect.tsx
@@ -1,51 +1,59 @@
 import * as React from "react"
-import { View, Text } from "react-native"
+import { View, Text, Image } from "react-native"
 import { Button } from "../views"
 import Spotify, { AuthState } from '../lib/spotify'
 import { Artist, Playlist, Track } from '../lib/spotify/types'
 import { Props } from '../lib/react/types'
-import color from "../lib/ui/color"
 import _ from 'underscore'
 import LinearGradient from 'react-native-linear-gradient'
+import { Svg, Defs, Path, Text as SvgText, TextPath } from "react-native-svg"
 
-const ARTISTS = [
+type ArtistConfig = {
+  name: string,
+  display: boolean,
+  displayName?: string,
+  image?: any,
+}
+
+const ARTISTS: ArtistConfig[] = [
   {
     name: "Robby Krieger",
-    color: "lightpink",
     displayName: "Robby Krieger of The Doors",
     display: true,
+    image: require('../images/robby_nft.png'),
   },
   {
     name: "Talib Kweli",
-    color: "lightgreen",
     display: true,
+    image: require('../images/kweli_nft.png'),
   },
   {
     name: "Tribe Friday",
-    color: "lightblue",
     display: true,
+    image: require('../images/tribefriday_nft.png'),
   },
   {
     name: "The Doors",
-    color: "lightgray",
     display: false,
   },
   {
     name: "Bartees Strange",
-    color: "green",
     display: true,
+    image: require('../images/bartees_nft.png'),
   },
   {
     name: "Revenge Wife",
-    color: "yellow",
     display: true,
+    image: require('../images/revengewife_nft.png'),
   },
   {
     name: "Christian Crosby",
-    color: "pink",
     display: true,
+    image: require('../images/crosby_nft.png'),
   }
 ]
+
+const DISPLAY_ARTISTS = ARTISTS.filter(a => a.display)
 
 const ARTIST_NAMES = new Set<string>(ARTISTS.map(a => a.name))
 
@@ -56,6 +64,15 @@ enum ViewState {
   Scored,
 }
 
+
+const ArtistImage = ({ source }: { source: any }) => {
+  return (
+    <View style={{ borderRadius: 131, width: 262, height: 262, overflow: 'hidden' }}>
+      <Image source={source} style={{ width: 360, height: 360, marginLeft: -49, marginRight: -49, marginTop: -69, marginBottom: -29 }} />
+    </View>
+  )
+}
+
 const Connect = ({ navigation }: Props<'Connect'>) => {
   const [viewState, setViewState] = React.useState<ViewState>(ViewState.Default)
 
@@ -64,13 +81,13 @@ const Connect = ({ navigation }: Props<'Connect'>) => {
   const [topTracks, setTopTracks] = React.useState<Track[]>([])
   const [myTracks, setMyTracks] = React.useState<Track[]>([])
   const [playlistTracks, setPlaylistTracks] = React.useState<Track[]>([])
+  const [matchedArtists, setMatchedArtists] = React.useState<Artist[]>([])
 
   const [dataSyncDone, setDataSyncDone] = React.useState(false)
-  const [scoreDone, setScoreDone] = React.useState(false)
-  const [scoringStarted, setScoringStarted] = React.useState(0)
 
   const [authState, setAuthState] = React.useState<AuthState>(AuthState.UNAUTHENTICATED)
   const [score, setScore] = React.useState(0)
+  const [displayingArtist, setDisplayingArtist] = React.useState(_.sample(DISPLAY_ARTISTS, 1)[0])
 
   React.useEffect(() => {
     setAuthState(Spotify.shared().authState)
@@ -78,8 +95,25 @@ const Connect = ({ navigation }: Props<'Connect'>) => {
   }, [])
 
   React.useEffect(() => {
+    if (dataSyncDone) {
+      console.log('done')
+      return
+    }
     if (authState === AuthState.AUTHENTICATED) {
       console.log('authenticated')
+      setViewState(ViewState.Scoring)
+      
+      const showScore = () => { setViewState(ViewState.Scored) }
+      let isTimeUp = false
+      let isScoringComplete = false
+      setTimeout(() => {
+        if (isScoringComplete) {
+          showScore()
+        } else {
+          isTimeUp = true
+        }
+      }, 10000)
+
       Promise.all([
         Spotify.shared().getFollowedArtists().then(as => { console.log('got followed artists', as.length); setFollowedArtists(as) } ),
         Spotify.shared().getTopArtists().then(as => { console.log('got top artists', as.length); setTopArtists(as) } ),
@@ -105,23 +139,36 @@ const Connect = ({ navigation }: Props<'Connect'>) => {
       .then(() => {
         setDataSyncDone(true)
       })
+      .then(() => {
+        if (isTimeUp) {
+          showScore()
+        } else {
+          isScoringComplete = true
+        }
+      })
     }
-  }, [authState])
+  }, [authState, dataSyncDone])
 
   React.useEffect(() => {
     if (!dataSyncDone) return
 
     console.log('scoring')
-    const followedArtistCount = followedArtists.filter(a => ARTIST_NAMES.has(a.name)).length
-    console.log('got', followedArtistCount, ' followed artists')
-    const topArtistCount = topArtists.filter(a => ARTIST_NAMES.has(a.name)).length
-    console.log('got', topArtistCount, ' top artists')
-    const topTrackCount = topTracks.filter(t => _.any(t.artists, a => ARTIST_NAMES.has(a.name))).length
-    console.log('got', topTrackCount, ' top tracks')
-    const myTrackCount = myTracks.filter(t => _.any(t.artists, a => ARTIST_NAMES.has(a.name))).length
-    console.log('got', myTrackCount, ' my tracks')
-    const playlistTrackCount = playlistTracks.filter(t => _.any(t.artists, a => ARTIST_NAMES.has(a.name))).length
-    console.log('got', playlistTrackCount, ' playlist tracks')
+    const followedMatches = followedArtists.filter(a => ARTIST_NAMES.has(a.name))
+    const followedArtistCount = followedMatches.length
+
+    const topArtistMatches = topArtists.filter(a => ARTIST_NAMES.has(a.name))
+    const topArtistCount = topArtistMatches.length
+    
+    const topTrackMatches = topTracks.filter(t => _.any(t.artists, a => ARTIST_NAMES.has(a.name)))
+    const topTrackCount = topTrackMatches.length
+
+    const myTrackMatches = myTracks.filter(t => _.any(t.artists, a => ARTIST_NAMES.has(a.name)))
+    const myTrackArtistMatches = myTrackMatches.map(t => t.artists.find(a => ARTIST_NAMES.has(a.name))).filter(x => !!x) as Artist[]
+    const myTrackCount = myTrackMatches.length
+
+    const playlistTrackMatches = playlistTracks.filter(t => _.any(t.artists, a => ARTIST_NAMES.has(a.name)))
+    const playlistArtistMatches = playlistTracks.map(t => t.artists.find(a => ARTIST_NAMES.has(a.name))).filter(x => !!x) as Artist[]
+    const playlistTrackCount = playlistTrackMatches.length
 
     const topCount = topArtistCount + topTrackCount
     const savedCount = myTrackCount + playlistTrackCount
@@ -132,10 +179,20 @@ const Connect = ({ navigation }: Props<'Connect'>) => {
 
     const score = topCount * TOP_SCORE + followedArtistCount * FOLLOWED_SCORE + savedCount * SAVED_SCORE
 
+    const matchesWithDupes = followedMatches.concat(topArtistMatches).concat(myTrackArtistMatches).concat(playlistArtistMatches)
+    console.log('have', matchesWithDupes.length, 'undeduplicated matches')
+    const matchedIds = new Set<string>()
+    const dedupedMatches: Artist[] = []
+    matchesWithDupes.forEach((a => {
+      if (matchedIds.has(a.id)) return
+      dedupedMatches.push(a)
+    }))
+    console.log('have', dedupedMatches.length, 'deduplicated matches')
+    setMatchedArtists(dedupedMatches)
+
     console.log('score', score)
 
     setScore(score)
-    setScoreDone(true)
   }, [dataSyncDone])
 
   const authenticate = () => {
@@ -152,21 +209,74 @@ const Connect = ({ navigation }: Props<'Connect'>) => {
 
   }
 
+  const StartScene = () => {
+    return (
+      <>
+        <Text style={{ marginTop: 100, fontSize: 80, color: 'white', textAlign: 'center', fontWeight: 'bold', textTransform: "uppercase" }}>Are you a true fan?</Text>
+        <Button onPress={authenticate} medium backgroundColor="white" textColor="#FF5CB8" textStyle={{ fontWeight: 'normal' }} style={{ width: 200, marginBottom: 50 }}>CHECK SPOTIFY</Button>
+      </>
+    )
+  }
+
+  const WaitingScene = () => {
+    React.useEffect(() => {
+      const interval = setInterval(() => {
+        setDisplayingArtist(_.sample(DISPLAY_ARTISTS, 1)[0])
+      }, 2000)
+      return () => {
+        clearInterval(interval)
+      }
+    }, [])
+
+    return (
+      <View style={{ flexDirection: 'column', justifyContent: 'space-around', alignItems: 'center', width: '100%', }}>
+        <Text style={{ color: 'white', fontWeight: '500', fontSize: 18, marginBottom: 18 }}>Checking Spotify</Text>
+        <Image style={{ width: 50, height: 50, marginBottom: 20 }} source={require('../images/spotify_logo.png')}/>
+        <ArtistImage source={displayingArtist.image}/>
+        <Text style={{ color: 'white', fontWeight: '500', fontSize: 18, marginTop: 30 }}>{displayingArtist.name}...</Text>
+      </View>
+    )
+  }
+
+  const ScoreScene = () => {
+    return (
+      <View style={{ flexDirection: 'column', justifyContent: 'space-around', alignItems: 'center', width: '100%', }}>
+        <Text style={{ fontSize: 40, fontWeight: '600', color: 'white', textTransform: 'uppercase' }}>You Scored!</Text>
+        <Text style={{ fontSize: 150, fontWeight: '800', color: 'white' }}>{score}</Text>
+        <Text style={{ fontSize: 16, color: 'white', width: 200, textAlign: 'center', lineHeight: 28}}>You are a fan of {matchedArtists.length} artist{matchedArtists.length === 1 ? '' : 's'} in the line up tonight!</Text>
+        <Text style={{ fontSize: 14, color: '#5244DF', width: 200, marginVertical: 20, textAlign: 'center'}}>{matchedArtists.map(a => a.name).join(' / ')}</Text>
+        <Text style={{ color: 'white', fontSize: 16, fontWeight: '500', marginTop: 20 }}>SCORING:</Text>
+        <Text style={{ color: 'white', fontSize: 14, marginTop: 8 }}>In a Playlist - 5 pt</Text>
+        <Text style={{ color: 'white', fontSize: 14, marginTop: 8 }}>Followed Artist - 10 pt</Text>
+        <Text style={{ color: 'white', fontSize: 14, marginTop: 8 }}>Top Artist - 20 pt</Text>
+      </View>
+    )
+  }
+
+  const textWorksButNoShadowSad = () => {
+    return (
+      <View style={{ width: 320, height: 40, shadowColor: 'black', shadowOffset: { width: 0, height: 0 }, shadowRadius: 10 }}>
+      <Svg width='100%' height={40}>
+        <Defs>
+          <Path id='bigArc' d='m 0 35 a 160 18, 0, 0, 1, 320 35'/>
+        </Defs>
+        <SvgText x='0' fill='#A595FC' fontSize={24} fontFamily="RubikMonoOne-Regular">
+          <TextPath xlinkHref="#bigArc">MUSIC UNITES US!</TextPath>
+        </SvgText>
+      </Svg>
+    </View>
+    )
+  }
+
   const centerContent = () => {
     switch (viewState) {
-      case ViewState.Default:
-        return (
-          <>
-            <Text style={{ marginTop: 100, fontSize: 80, color: 'white', textAlign: 'center', fontWeight: 'bold', textTransform: "uppercase" }}>Are you a true fan?</Text>
-            <Button onPress={authenticate} medium backgroundColor="white" textColor="#FF5CB8" textStyle={{ fontWeight: 'normal' }} style={{ width: 200, marginBottom: 50 }}>CHECK SPOTIFY</Button>
-          </>
-        )
+      case ViewState.Default: 
       case ViewState.Authenticating:
-        return null
+        return <StartScene/>
       case ViewState.Scoring:
-        return null
+        return <WaitingScene/>
       case ViewState.Scored:
-        return null
+        return <ScoreScene/>
       default:
         return null
     }
@@ -183,9 +293,9 @@ const Connect = ({ navigation }: Props<'Connect'>) => {
         <View style={{ flexDirection: 'row', justifyContent: 'flex-end', width: '100%', paddingRight: 20, paddingVertical: 20 }}>
           <Button onPress={share} medium backgroundColor="white" textColor="#550451" textStyle={{ fontWeight: 'normal' }} style={{ opacity: 0.4, width: 100 }}>SHARE</Button>
         </View>
-        <Text style={{ fontSize: 24, color: '#A595FC', fontFamily: "RubikMonoOne-Regular" }}>MUSIC UNITES US!</Text>
+        <Text style={styles.musicUnitesUs}>MUSIC UNITES US!</Text>
       </View>
-      <View style={{ flexGrow: 1, paddingHorizontal: 30, flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center' }}>
+      <View style={{ flexGrow: 1, paddingHorizontal: 30, flexDirection: 'column', justifyContent: 'space-around', alignItems: 'center' }}>
         {centerContent()}
       </View>
 
@@ -194,6 +304,29 @@ const Connect = ({ navigation }: Props<'Connect'>) => {
       </View>
     </LinearGradient>
   )
+}
+
+const styles = {
+  musicUnitesUs: {
+    fontSize: 24,
+    color: '#B0AAFF',
+    fontFamily: "RubikMonoOne-Regular",
+    textShadowColor: 'rgba(176, 170, 255, 0.75)',
+    textShadowOffset: { width: 0, height: 0 },
+    textShadowRadius: 10
+  },
+
+  smallText: {
+    fontSize: 16
+  },
+
+  mediumText: {
+    fontSize: 20
+  },
+
+  largeText: {
+    fontSize: 24
+  },
 }
 
 export default Connect
