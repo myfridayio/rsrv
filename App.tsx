2b597856b7ec2bc4c19c82ceb2c4a1d74e4dc27c
Kiril Savino <kiril.savino@gmail.com>
2023-03-14 19:08:14 -0400

    remove unused shit

--- App.tsx
+++ App.tsx
@@ -1,17 +1,6 @@
 import * as React from "react"
 import { NavigationContainer } from "@react-navigation/native"
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
-import HomeScreen from './Home'
-import AboutScreen from './About'
-import CreateWallet from "./CreateWallet"
-import WalletAddressScreen from "./WalletAddress"
-import TwitterConnectScreen from "./TwitterConnect"
-import MoreTokensConnectScreen from "./MoreTokensConnect"
-import TwitterScreen from "./TwitterScreen"
-import Dashboard from "./Dashboard"
-import NetflixConnect from "./NetflixConnect"
-import Mercedes from './Mercedes'
-import Menu from './Menu'
 import { Connect, NFTs } from "./pages"
 
 const Stack = createNativeStackNavigator();
@@ -20,15 +9,6 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Connect" screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="About" component={AboutScreen} />
-        <Stack.Screen name="CreateWallet" component={CreateWallet} />
-        <Stack.Screen name="WalletAddress" component={WalletAddressScreen} />
-        <Stack.Screen name="TwitterConnect" component={TwitterConnectScreen} />
-        <Stack.Screen name="Dashboard" component={Dashboard} />
-        <Stack.Screen name="NetflixConnect" component={NetflixConnect} />
-        <Stack.Screen name="Mercedes" component={Mercedes} />
-        <Stack.Screen name="Menu" component={Menu} />
         <Stack.Screen name="Connect" component={Connect} />
         <Stack.Screen name="NFTs" component={NFTs} />
       </Stack.Navigator>
--- Types.ts
+++
@@ -1,6 +0,0 @@
-export interface FakeNav {
-    navigate: (route: string, args?: {}) => void
-    goBack: () => void
-    reset: (config: {}) => void
-    addListener: (key: string, f: () => void) => (() => void)
-}
